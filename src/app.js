d25abda80ec701660647d363db5cae6f493c01b7
enriquetoscanoh94 <enriquetoscanoh94@icloud.com>
2026-04-28 21:22:48 +0000

    se creo el generador de tarjetas

--- src/app.js
+++ src/app.js
@@ -1,11 +1,26 @@
-import "bootstrap";
-import "./style.css";
 
+const generarNuevaCarta = () => {
+  const palos = [
+    { simbolo: "♥", clase: "heart" },
+    { simbolo: "♦", clase: "diamond" },
+    { simbolo: "♠", clase: "spade" },
+    { simbolo: "♣", clase: "club" }
+  ];
+  const valores = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  const paloRandom = palos[Math.floor(Math.random() * palos.length)];
+  const valorRandom = valores[Math.floor(Math.random() * valores.length)];
+
+  document.querySelector("#card").className = "card " + paloRandom.clase;
+  document.querySelector("#arribasimbolo").innerHTML = paloRandom.simbolo;
+  document.querySelector("#valorcentral").innerHTML = valorRandom;
+  document.querySelector("#abajosimbolo").innerHTML = paloRandom.simbolo;
+};
+
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  generarNuevaCarta(); 
+
+ 
+  document.querySelector("#btn-nueva-carta").addEventListener("click", generarNuevaCarta);
 };
